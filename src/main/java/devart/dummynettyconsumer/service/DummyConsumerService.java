1881311dca3dd2b25000a86b747ab0190a0b8cfb
Artyom Panfutov <artyom.cox@gmail.com>
2021-02-07 21:52:48 +0300

    removed postconstruct method

--- src/main/java/devart/dummynettyconsumer/service/DummyConsumerService.java
+++ src/main/java/devart/dummynettyconsumer/service/DummyConsumerService.java
@@ -4,16 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.annotation.PostConstruct;
-
 @Service
 public class DummyConsumerService {
     private final WebClient webClient;
 
-    @PostConstruct
-    public void init () {
-        System.out.println("GOGOGOGOG");
-    }
     @Autowired
     public DummyConsumerService(WebClient webClient) {
         this.webClient = webClient;
